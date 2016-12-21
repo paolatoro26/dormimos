2ad4950613eb7cb9bfcea379bfb2fa4f69e24d3c
paola <paolatorobejar@gmail.com>
2016-12-21 15:01:33 -0500

    index terminado

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dormimos
 dormimos
+
+Inicio:Dormimos
+
+## Ingreso de datos
+- 
+- weekday
+- vacation
+- puede_dormir
+
+## Proceso
+- si vacation es verdadero 
+	mostrar 1
+- si no 
+	si weekday es verdadero 
+		mostrar 0
+	si no	
+		mostrar 0
+	Fin si
+  Fin si
+
+*Fin*
--- js/main.js
+++ js/main.js
@@ -1,2 +1,44 @@
 	
 	
+function calcular()
+{
+	var vacation = document.getElementById("vacaciones").value;
+	var weekday = parseInt(document.getElementById("dia").value);
+
+	var isVacation=true;
+	var isWeekday=false;
+
+	var salida = document.getElementById("salida");
+
+	if (vacation =="1")
+	{
+		isVacation=false
+	};
+
+	if (weekday<6) 
+	{
+		isWeekday=true;
+	};
+
+
+	if (isVacation) 
+	{
+		salida.innerHTML = "1";
+	}
+	else
+	{
+		if (isWeekday) 
+		{
+			salida.innerHTML="0";
+		}
+		else{
+			salida.innerHTML="1";
+		}
+	}
+}	
+		
+
+
+
+
+	
